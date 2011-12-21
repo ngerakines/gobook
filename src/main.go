123b4950b2be5aee22ebf030eb3eac77c33cc612
Nick Gerakines <nick@gerakines.net>
2011-12-21 08:52:21 -0800

    Added some migration logic for going to mongodb from mysql.

--- src/main.go
+++ src/main.go
@@ -41,7 +41,7 @@ func displayArchive(req *web.Request) {
 
 	c := session.DB("gobook").C("items")
 	var result []*Item
-	iter := c.Find(nil).Sort(bson.M{ "when": 1}).Limit(250).Iter()
+	iter := c.Find(nil).Sort(bson.M{ "when": -1}).Limit(250).Iter()
 	c_err := iter.All(&result)
 
 	if c_err != nil {
@@ -57,7 +57,7 @@ func displayArchive(req *web.Request) {
 	log.Println(groupedItems)
 
 	flatItemGroups := flattenItemGroups(groupedItems)
-	log.Println(flatItemGroups)
+	// log.Println(flatItemGroups)
 
 	w := req.Respond(web.StatusOK, web.HeaderContentType, "text/html; charset=\"utf-8\"")
 	params := make(map[string]interface{})
@@ -97,6 +97,65 @@ func displayEntry(req *web.Request) {
         io.WriteString(w, RenderFile("templates/entry.html", params))
 }
 
+func displayMonth(req *web.Request) {
+	w := req.Respond(web.StatusOK, web.HeaderContentType, "text/html; charset=\"utf-8\"")
+	io.WriteString(w, RenderFile("templates/month.html", map[string]string{}))
+}
+
+func migrate(req *web.Request) {
+
+	err := db.Query("select id, message, date from entries")
+	if err != nil {
+		panic(err)
+	}
+	result, err := db.StoreResult()
+	if err != nil {
+		panic(err)
+	}
+	itemIndex := make(map[string]Item, result.RowCount())
+	for _, entry_row := range result.FetchRows() {
+		id := string( []uint8( entry_row[0].([]uint8) ) )
+		message := string([]uint8(entry_row[1].([]uint8)))
+		when := entry_row[2].(int64)
+		itemIndex[id] = Item{message, "", make([]string, 0), bson.MongoTimestamp(when) }
+	}
+	db.FreeResult()
+
+	for id, item := range itemIndex {
+		tags := getTags(id)
+		// log.Println(id, tags)
+		// item.Tags = tags
+		// itemIndex[id] = item
+		createItem(item.Message, "", tags, int64(item.When))
+	}
+
+	log.Println(itemIndex)
+
+	w := req.Respond(web.StatusOK, web.HeaderContentType, "text/html; charset=\"utf-8\"")
+	io.WriteString(w, "OK")
+
+}
+
+func getTags(id string) []string {
+	// NKG: Convert to prepared statement.
+	err := db.Query("select tag from tags where id = \"" + id + "\"")
+	if err != nil {
+		panic(err)
+	}
+	result, err := db.StoreResult()
+	if err != nil {
+		panic(err)
+	}
+	tags := make([]string, result.RowCount())
+	for index, row := range result.FetchRows() {
+		tag := string([]uint8( row[0].([]uint8)  ))
+		tags[index] = tag
+	}
+	db.FreeResult()
+	return tags
+}
+
+
 func main() {
 	/* log.Println(splitTags("Nick Carolyn Vanessa Hannah"))
 	log.Println(splitTags("\"Hello World\""))
@@ -123,10 +182,12 @@ func main() {
 	h := web.FormHandler(10000, false,
 		web.NewRouter().
 			Register("/", "GET", displayIndex, "POST", createEntry).
+			Register("/migrate", "GET", migrate).
 			Register("/archive", "GET", displayArchive).
 			Register("/tag/<tag:.*>", "GET", displayTag).
 			// Register("/entry/<id:.*>", "GET", displayEntry).
 			Register("/api/tag/rename/", "POST", renameTag).
+			Register("/summary/<year:.*>/<month:.*>", "GET", displayMonth).
 			Register("/static/<path:.*>", "GET", web.DirectoryHandler("./static/", new(web.ServeFileOptions))))
 	server.Run(port, h)
 }
